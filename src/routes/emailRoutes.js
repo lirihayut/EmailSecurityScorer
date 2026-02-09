a5d4b5393301fcacd400715972a5c2b174343210
lirihayut <lirihayut@gmail.com>
2026-02-09 20:15:04 +0200

    Add getBlacklist route to emailRoutes
    Added a new route to retrieve the blacklist.

--- src/routes/emailRoutes.js
+++ src/routes/emailRoutes.js
@@ -2,12 +2,14 @@ import express from 'express';
 import {
   analyzeEmail,
   addToBlacklist,
-  removeFromBlacklist
+  removeFromBlacklist,
+  getBlacklist
 } from '../controllers/emailController.js';
 
 const router = express.Router();
 
 router.post('/scan', analyzeEmail);
+router.get('/settings/blacklist', getBlacklist);
 router.post('/settings/blacklist', addToBlacklist);
 router.delete('/settings/blacklist/:email', removeFromBlacklist);
 

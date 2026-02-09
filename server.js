b91e08882fc3d51d56ac8be03beb87c9e6ef5035
lirihayut <lirihayut@gmail.com>
2026-02-09 20:15:25 +0200

    Enhance server setup with dotenv and cors
    Added dotenv for environment variable management and cors for handling cross-origin requests. Updated server setup to include a root route.

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import cors from 'cors'; 
+import dotenv from 'dotenv';
 import emailRoutes from './src/routes/emailRoutes.js';
 
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 app.use(express.json());
+
+// Routes
 app.use('/api/v1', emailRoutes);
-app.listen(3000, () => console.log('Server running on port 3000'));
-;
+
+app.get('/', (req, res) => {
+    res.send('Malicious Email Scorer API is running...');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
